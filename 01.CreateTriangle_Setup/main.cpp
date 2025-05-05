7f02c6ee207aacb6d8fbdac097cd10ccb72da997
neptunehjf <neptunehjf@sina.com>
2025-05-05 12:10:36 +0900

    01.CreateTriangle_Setup Instance

---
+++ 01.CreateTriangle_Setup/main.cpp
@@ -0,0 +1,124 @@
+﻿#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    GLFWwindow* window;
+    VkInstance instance;
+
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // GLFWが自動的にOpenGLコンテキストを作成しないように設定する
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan()
+    {
+        createInstance();
+    }
+
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup()
+    {
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+
+    void createInstance()
+    {
+        // VkApplicationInfo は必須ではないが、ドライバの最適化に利用される可能性がある
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        // VkInstanceCreateInfo は必須入力項目
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        // グローバル拡張情報
+        uint32_t glfwExtCount = 0;
+        const char** glfwExtNames = glfwGetRequiredInstanceExtensions(&glfwExtCount);
+        createInfo.enabledExtensionCount = glfwExtCount;
+        createInfo.ppEnabledExtensionNames = glfwExtNames; // const char** → const char* const* への暗黙的型変換
+        createInfo.enabledLayerCount = 0;
+
+        // 現在のruntimeでサポートされている拡張を表示
+        uint32_t extCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extCount, nullptr);
+        vector<VkExtensionProperties> extensions(extCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extCount, extensions.data());
+        cout << "Available Extension:" << endl;
+        for (const auto& extension : extensions) // auto& の使用は auto より効率的 (コピーコスト削減)
+        {
+            cout << "\t" << extension.extensionName << endl;
+        }
+
+        // GLFW が必要とする拡張を表示
+        cout << "Required GLFW Extension:" << endl;
+        for (int i = 0; i < glfwExtCount; i++)
+        {
+            cout << "\t" << glfwExtNames[i] << endl;
+        }
+
+        // インスタンスの作成
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw runtime_error("failed to create instance!");
+        }
+    }
+};
+
+int main() 
+{
+    HelloTriangleApplication app;
+
+    try 
+    {
+        app.run();
+    }
+    catch (const std::exception& e) 
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Vulkan
+
+directory
+..\Dependency\Includes
+E:\VulkanSDK\1.4.309.0\Include
+
+lib
+glfw3.lib
+vulkan-1.lib
